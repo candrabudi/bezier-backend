08eaa0e6bef8eec7a1a5983c0febebf93f4109cf
chandra <candrasachio@gmail.com>
2023-05-20 12:30:27 +0700

    Fix pagination on report tasks

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -5,16 +5,17 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\TaskClient;
 use Illuminate\Http\Request;
-
+use Illuminate\Pagination\LengthAwarePaginator;
 class ReportController extends Controller
 {
     public function listReportTask(Request $request)
     {
         try{
-
             $search = $request->search;
             $filter = $request->filter;
-            $paginate = $request->paginate ?? 6;
+            $page = $request->page ? $request->page : 1;
+            $limits = $request->total_per_page ? $request->total_per_page : 6;
+            $offset = ($page - 1) * $limits;
             $taskClients = TaskClient::when($search, function ($query) use ($search) {
                     return $query->where('users.name', 'LIKE', '%' . $search . '%');
                 })
@@ -23,16 +24,44 @@ class ReportController extends Controller
                     return $query->where('task_clients.status', $filter);
                 })
                 ->select('users.id as id', 'users.full_name', 'task_clients.status', 'received_at', 'submit_at')
-                ->with('memberAssign')
-                ->orderBy('id', 'ASC')
-                ->paginate($paginate);
+                ->with('memberAssign');
+
+            $tasksCount = $taskClients->count();
+            $taskData = $taskClients->orderBy('id', 'ASC')
+                                ->limit($limits)
+                                ->offset($offset)
+                                ->get();
+
+            $datas = [];
+            foreach($taskData as $task){
+                $member_assigns = [];
+                foreach($task->memberAssign->toArray() as $member_assign){
+                    $data_member = array(
+                        'full_name' => $member_assign['full_name'],
+                        'photo_profile_url' => !empty($member_assign['photo_profile_url']) ? url(\Storage::url($member_assign['photo_profile_url'])) : null
+                    );
+
+                    array_push($member_assigns, $data_member);
+                }
+                $data = array(
+                    'id' => $task->id,
+                    'full_name' => $task->full_name, 
+                    'status' => $task->status,
+                    'received_at' => $task->received_at,
+                    'submit_at' => $task->submit_at,
+                    'member_assign' => $member_assigns
+                );
+
+                array_push($datas, $data);
+            }
+            $allTasks = new LengthAwarePaginator($datas, $tasksCount, $limits);
             return response()->json([
                 'meta'  => [
                     'status'    => 'success',
                     'code'      => 200,
                     'message'   => 'Successfully get data design library'
                 ],
-                'data'  => $taskClients
+                'data'  => $allTasks
             ], 200);
 
         }catch(\Exception $e){
--- app/Models/TaskClient.php
+++ app/Models/TaskClient.php
@@ -4,11 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Pagination\Paginator;
 class TaskClient extends Model
 {
     use HasFactory;
 
+    public static function customPaginate($perPage = 10)
+    {
+        $page = Paginator::resolveCurrentPage('page');
+
+        $results = self::query()
+            ->forPage($page, $perPage)
+            ->with('memberAssign')
+            ->get();
+
+        $total = self::query()->count();
+
+        return new Paginator($results, $perPage, $page, [
+            'path' => Paginator::resolveCurrentPath(),
+            'total' => $total,
+        ]);
+    }
+    
     public function memberAssign()
     {
         return $this->hasMany(MemberAssign::class, 'client_user_id', 'id')
